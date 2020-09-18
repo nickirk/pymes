74822d2b26752e2471f982216bbcc4544921c737
Ke Liao <ke.liao.whu@gmail.com>
2020-09-18 17:01:07 +0200

    uMat is now correct

--- model/ueg.py
+++ model/ueg.py
@@ -29,6 +29,10 @@ class UEG:
         self.cutoff = 0.
 
         self.basis_indices_map = None
+        
+        self.kPrime = None
+
+        self.correlator = None
 
 
     def is_k_in_basis(self,k):
@@ -90,15 +94,18 @@ class UEG:
     
         return basis_fns
 
-    def evalCoulIntegrals(self):
+    def eval2BodyIntegrals(self, correlator = None, dtype=np.float64,sp=1):
         world = ctf.comm()
         algoName = "UEG.evalCoulIntegrals"
 
         if self.basis_fns == None:
             raise BasisSetNotInitialized(algoName)
+
+        if correlator is not None:
+            self.correlator = correlator
     
         nP = int(len(self.basis_fns)/2)
-        tV_pqrs = ctf.tensor([nP,nP,nP,nP], dtype=complex, sp=1)
+        tV_pqrs = ctf.tensor([nP,nP,nP,nP], dtype=dtype, sp=sp)
         indices = []
         values = []
         
@@ -113,6 +120,120 @@ class UEG:
         startTime = time.time()
         for p in range(nP):
             if (p) % world.np() == rank:
+                if rank == 0:
+                    print("\tElapsed time=%d s: %i in %i finished" % (time.time()-startTime, p, nP))
+                for r in range(nP):
+                    dIntK = self.basis_fns[p*2].k-self.basis_fns[r*2].k
+                    dKVec = self.basis_fns[p*2].kp-self.basis_fns[r*2].kp
+                    uMat  = 0.
+                    if correlator is not None:
+                        uMat = self.sumNablaUSquare(dKVec) 
+                        print("dK=", dIntK, uMat)
+
+                    for q in range(nP):
+                        intKS = self.basis_fns[q*2].k + dIntK
+                        #if self.is_k_in_basis(intKS):
+                        locS = numKInEachDir**2 * (intKS[0] + self.imax) + \
+                                numKInEachDir * (intKS[1] + self.imax) +\
+                                intKS[2] + self.imax
+                        if locS < len(self.basis_indices_map):
+                            s = int(self.basis_indices_map[locS])
+                            if s < 0:
+                                continue
+                        else:
+                            continue
+                        dkSquare = dKVec.dot(dKVec)
+                        if correlator is None:
+                            if np.abs(dkSquare) > 0.:
+                                w = 4.*np.pi/dkSquare/self.Omega
+                                indices.append(nP**3*p + nP**2*q + nP*r + s)
+                                values.append(w)
+                        else:
+                            # tc
+                            if np.abs(dkSquare) > 0.:
+                                rs_dk = self.basis_fns[r*2].kp-self.basis_fns[s*2].kp
+                                w = 4.*np.pi/dkSquare \
+                                        + dkSquare * correlator(dkSquare)\
+                                        - (rs_dk.dot(-dKVec)) * correlator(dkSquare) \
+                                        +  uMat
+                                w = w / self.Omega
+                            else:
+                                w = correlator(dkSquare, multKSquare=True)- uMat
+                                w = w / self.Omega
+                                # \sum_k' (k-k')k'u(k-k')u(k')
+
+                            indices.append(nP**3*p + nP**2*q + nP*r + s)
+                            values.append(w)
+                            #tV_pqrs[p,q,r,s] = 4.*np.pi/dkSquare/sys.Omega
+        tV_pqrs.write(indices,values)
+        return tV_pqrs
+
+    def sumNablaUSquare(self, k, cutoff=20):
+        # need to test convergence of this cutoff
+        if self.kPrime is None:
+            self.kPrime = np.array([[i,j,k] for i in range(-cutoff,cutoff+1) \
+                    for j in range(-cutoff,cutoff+1) for k in range(-cutoff,cutoff+1)])
+        k1 = 2*np.pi*self.kPrime/self.L
+        k2 = k - k1
+
+        k1Square = np.einsum("ni,ni->n", k1,k1)
+        k2Square = np.einsum("ni,ni->n", k2,k2)
+        k1DotK2 = np.einsum("ni,ni->n", k1,k2)
+        #print(self.correlator(k1Square))
+        #print(self.correlator(k2Square))
+        result = k1DotK2 * self.correlator(k1Square) * self.correlator(k2Square)
+        result = np.einsum("n->", result) / self.Omega
+
+        return result
+
+    #def calcNablaUMat(self):
+    #    nablaUMat = np.zeros((2*self.imax))
+
+
+
+
+
+    def yukawa(self, kSquare, multKSquare=False):
+        '''
+        The G=0 terms need more consideration
+        '''
+        rho = self.nel / self.Omega 
+        muSquare = np.sqrt(4.*np.pi*rho)
+        a = -4.*np.pi
+        if not multKSquare:
+            b = kSquare*(kSquare+muSquare*2)
+            result = np.divide(a , b, out = np.zeros_like(b), where=np.abs(b)>1e-12)
+        else:
+            result = a/(kSquare+muSquare*2)
+
+        return result
+
+
+
+    def evalTransCorr2BodyIntegrals(correlator):
+    
+        world = ctf.comm()
+        algoName = "UEG.evalTransCorr2BodyIntegrals"
+
+        if self.basis_fns == None:
+            raise BasisSetNotInitialized(algoName)
+    
+        nP = int(len(self.basis_fns)/2)
+        tTC_pqrs = ctf.tensor([nP,nP,nP,nP], dtype=dtype, sp=sp)
+        indices = []
+        values = []
+        
+        #basisKp = [ self.basis_fns[2*i].kp.tolist() for i in range(nP)]
+        
+    
+        rank = world.rank()
+        numKInEachDir = self.imax*2+1
+        if rank == 0:
+            print(algoName)
+
+        startTime = time.time()
+        for p in range(nP):
+            if p % world.np() == rank:
                 if rank == 0:
                     print("\tElapsed time=%d s: %i in %i finished" % (time.time()-startTime, p, nP))
                 for r in range(nP):
@@ -136,7 +257,13 @@ class UEG:
                             values.append(4.*np.pi/dkSquare/self.Omega)
                             #tV_pqrs[p,q,r,s] = 4.*np.pi/dkSquare/sys.Omega
         tV_pqrs.write(indices,values)
-        return tV_pqrs
+    
+        return tTC_pqrs
+    
+    def evalTransCorrThreeBodyIntegrals(tV_pqrs, basis_fns, correlator):
+    
+    
+        return tTC_opqrst
 
     def calcGamma(self, overlap_basis, nP):
         '''
