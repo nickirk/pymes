b0fc56c4063673146fbb479de7a0147ad404df55
Ke Liao <ke.liao.whu@gmail.com>
2021-01-29 17:50:06 +0100

    added symmetrized 3-body integrals as a test file

---
+++ test/test_3body_symmetric_tcdump.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3 -u
+
+import time
+import numpy as np
+import sys
+
+sys.path.append("/home/liao/Work/Research/TCSolids/scripts/")
+
+import ctf
+from ctf.core import *
+
+import pymes
+from pymes.model import ueg
+from pymes.util import tcdump
+
+## Script for generating plane wave basis
+## hamiltonian integrals for unifrom electron gas
+
+##############################################################################
+#   1. ctf tensors starts with a lower case t
+#   2. indices follow each variable after a _
+#   3. numpy's nparray runs fastest with the right most index
+#   4. for small tensors, use nparrays, only when large contractions are needed
+#      then use ctf tensors. In case in the future some other tensor engines
+#      might be used
+##############################################################################
+
+
+def main(nel, cutoff,rs, gamma, kc, tc):
+    world=ctf.comm()
+    nel = nel
+    no = int(nel/2)
+    nalpha = int(nel/2)
+    nbeta = int(nel/2)
+    rs = rs
+
+    # Cutoff for the single-particle basis set.
+    cutoff = cutoff
+
+    # Symmetry of the many-particle wavefunction: consider gamma-point only.
+    timeSys = time.time()
+    ueg_model = ueg.UEG(nel, nalpha, nbeta, rs)
+    if world.rank() == 0:
+        print("%i electrons" % nel)
+        print("rs=", rs)
+        print("Volume of the box : %f " % ueg_model.Omega)
+        print("Length of the box : %f " % ueg_model.L)
+        print("%f.3 seconds spent on setting up system" % (time.time()-timeSys))
+
+
+    timeBasis = time.time()
+    ueg_model.init_single_basis(cutoff)
+
+    nSpatialOrb = int(len(ueg_model.basis_fns)/2)
+    nP = nSpatialOrb
+    nGOrb = nSpatialOrb
+
+    nv = nP - no
+    if world.rank() == 0:
+        print('# %i Spin Orbitals\n' % int(len(ueg_model.basis_fns)))
+        print('# %i Spatial Orbitals\n' % nSpatialOrb)
+        print("%f.3 seconds spent on generating basis." % (time.time()-timeBasis))
+
+
+    timeCoulInt = time.time()
+    ueg_model.gamma = gamma
+
+    ueg_model.kCutoff = ueg_model.L/(2*np.pi)*2.3225029893472993/rs
+    if world.rank() == 0:
+        print("kCutoff=",ueg_model.kCutoff)
+
+    # writing in chemists form the 3-body integral is
+    # (or|ps|qt)
+
+    tV_opqrst = ueg_model.eval3BodyIntegrals(correlator=ueg_model.trunc,sp=1)
+    # symmetrize the 3-body integrals
+    tV_sym_opqrst = ctf.tensor([nSpatialOrb,nSpatialOrb,nSpatialOrb,nSpatialOrb,\
+                                nSpatialOrb,nSpatialOrb], sp=tV_opqrst.sp)
+    print("symmetrizing 3-body integral")
+    print("size of 3-body integral=", tV_sym_opqrst.size)
+    tV_sym_opqrst.i("opqrst") <<  tV_opqrst.i("opqrst") +tV_opqrst.i("oqprts")\
+                                     +tV_opqrst.i("qpotsr")
+
+    print("Writing 3-body integrals into TCDUMP")
+    tcdump.write2Tcdump(tV_sym_opqrst)
+
+
+
+if __name__ == '__main__':
+    #for gamma in None:
+    gamma = None
+    nel = 14
+    for rs in [0.5]:
+        for cutoff in [1]:
+            kCutoffFraction = None
+            for tc in [True]:
+                main(nel,cutoff,rs, gamma, kCutoffFraction,tc)
+    ctf.MPI_Stop()
--- util/tcdump.py
+++ util/tcdump.py
@@ -4,27 +4,28 @@ import numpy as np
 
 
 
-def write2Tcdump(integrals, dtype='r'):
+def write2Tcdump(tV_opqrst, dtype='r'):
     world = ctf.comm()
 
-    nOrb = integrals.shape[0]
+    nOrb = tV_opqrst.shape[0]
 
-    # (ia|jb|kc)
+    # (or|ps|qt)
+
     if world.rank() == 0:
         f = open("TCDUMP", "w")
-        inds, vals = integrals.read_all_nnz()
+        inds, vals = tV_opqrst.read_all_nnz()
         for l in range(len(inds)):
-            i = int(inds[l]/nOrb**5)
-            a = int((inds[l]-i*nOrb**5)/nOrb**4)
-            j = int((inds[l]-i*nOrb**5-a*nOrb**4)/nOrb**3)
-            b = int((inds[l]-i*nOrb**5-a*nOrb**4-j*nOrb**3)/nOrb**2)
-            k = int((inds[l]-i*nOrb**5-a*nOrb**4-j*nOrb**3-b*nOrb**2)/nOrb)
-            c = int(inds[l]-i*nOrb**5-a*nOrb**4-j*nOrb**3-b*nOrb**2-k*nOrb)
+            o = int(inds[l]/nOrb**5)
+            p = int((inds[l]-o*nOrb**5)/nOrb**4)
+            q = int((inds[l]-o*nOrb**5-p*nOrb**4)/nOrb**3)
+            r = int((inds[l]-o*nOrb**5-p*nOrb**4-q*nOrb**3)/nOrb**2)
+            s = int((inds[l]-o*nOrb**5-p*nOrb**4-q*nOrb**3-r*nOrb**2)/nOrb)
+            t = int(inds[l]-o*nOrb**5-p*nOrb**4-q*nOrb**3-r*nOrb**2-s*nOrb)
             if np.abs(vals[l]) > 1e-10:
             # depends on the correlator, sometimes there are 0 integrals when
             # there is a k=0. But in general, this is not the case
-                f.write(str(vals[l])+" "+str(i+1)+" "+\
-                        str(j+1)+" "+str(k+1)+" "+str(a+1)+" "+str(b+1)+" "\
-                        +str(c+1)+"\n")
+                f.write(str(vals[l])+" "+str(o+1)+" "+\
+                        str(p+1)+" "+str(q+1)+" "+str(r+1)+" "+str(s+1)+" "\
+                        +str(t+1)+"\n")
         f.close()
     return
