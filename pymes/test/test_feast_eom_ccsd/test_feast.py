ec721abb43a34752df7478d5479a63f99180ef0c
keliao <ke.liao.whu@gmail.com>
2024-04-19 23:50:30 +0200

    Phasing out ctf in feast; nonsymetric model ham works correctly

--- pymes/test/test_feast_eom_ccsd/test_feast.py
+++ pymes/test/test_feast_eom_ccsd/test_feast.py
@@ -6,7 +6,7 @@ from pymes.solver import ccsd, feast_eom_ccsd
 from pymes.mean_field import hf
 from pymes.integral.partition import part_2_body_int
 
-def driver(fcidump_file="pymes/test/test_eom_ccsd/FCIDUMP.LiH.321g", 
+def driver(fcidump_file="pymes/test/test_eom_ccsd/FCIDUMP.LiH.sto6g", 
            ref_e={  "hf_e": -7.92958534362757, 
                     "ccsd_e": -0.0190883270951031,
                     "ee": [0.1180867117168979, 0.154376205595602]}):
@@ -47,17 +47,17 @@ def driver(fcidump_file="pymes/test/test_eom_ccsd/FCIDUMP.LiH.321g",
     dict_t_V_dressed = mycc.get_T1_dressed_V(t_T_ai, dict_t_V)#, dict_t_V_dressed)
 
     n_e = 2
-    eom_cc = feast_eom_ccsd.FEAST_EOM_CCSD(no, e_c=0.15, e_r=0.02, n_trial=2, max_iter=100, tol=1e-8)
+    eom_cc = feast_eom_ccsd.FEAST_EOM_CCSD(no, e_c=0.184, e_r=0.01, n_trial=2, max_iter=100, tol=1e-8)
     e_excit = eom_cc.solve(t_fock_dressed_pq, dict_t_V_dressed, t_T_abij)
     print("Excited state energies = ", e_excit)
     e_excit_ref = ref_e["ee"]
     assert np.allclose(e_excit, e_excit_ref[:n_e])
 
-def test_feast():
+def test_feast_model_ham():
     no = 2
     eom_cc = feast_eom_ccsd.FEAST_EOM_CCSD(no, e_c=3.5, e_r=1, n_trial=2, max_iter=100, tol=1e-8)
     eom_cc.solve_test(nv=4)
 
 if __name__ == "__main__":
-    #test_feast()
-    driver()
+    test_feast_model_ham()
+    #driver()
