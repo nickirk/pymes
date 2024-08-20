be06a6460636c06c85b70566034a250b0d29035d
Ke Liao <ke.liao.whu@gmail.com>
2024-08-20 18:32:54 +0200

    add cifrt_eom solver

--- pymes/solver/rt_eom_rccsd.py
+++ pymes/solver/rt_eom_rccsd.py
@@ -1,6 +1,6 @@
 """
 This implementation uses functions from pyscf 
-to achieve the FEAST-EOM-CCSD algorithm.
+to achieve the Real Time FEAST-EOM-CCSD algorithm.
 """
 
 import numpy as np
@@ -17,7 +17,7 @@ from pyscf import __config__
 from pymes.log import print_title, print_logging_info
 from pymes.solver.feast_eom_ccsd import get_gauss_legendre_quadrature
 
-def feast(eom, nroots=1, e_r=None, e_c=None, koopmans=False, guess=None, left=False, eris=None, imds=None, **kwargs):
+def kernel(eom, dt=0.1, e_r=None, e_c=None, koopmans=False, guess=None, left=False, eris=None, imds=None, **kwargs):
     cput0 = (logger.process_clock(), logger.perf_counter())
     log = logger.Logger(eom.stdout, eom.verbose)
     if eom.verbose >= logger.WARN:
@@ -30,7 +30,7 @@ def feast(eom, nroots=1, e_r=None, e_c=None, koopmans=False, guess=None, left=Fa
     matvec, diag = eom.gen_matvec(imds, left=left, **kwargs)
 
     size = eom.vector_size()
-    nroots = min(nroots, size)
+    nroots = 1
     # create initial guesses
     print_logging_info("Initialising u tensors...", level=1)
     if guess is not None:
@@ -38,6 +38,7 @@ def feast(eom, nroots=1, e_r=None, e_c=None, koopmans=False, guess=None, left=Fa
         for g in guess:
             assert g.size == size
     else:
+        print_logging_info("No guess provided. Generating random guess.")
         user_guess = False
         guess = eom.get_init_guess(nroots, koopmans, diag)
 
@@ -47,104 +48,49 @@ def feast(eom, nroots=1, e_r=None, e_c=None, koopmans=False, guess=None, left=Fa
     # GHF or customized RHF/UHF may be of complex type
     real_system = (eom._cc._scf.mo_coeff[0].dtype == np.double)
 
-    print_title("FEAST-EOM-CCSD Solver")
+    print_title("CIFRT-EOM-CCSD Solver")
     time_init = time.time()
 
     u_vec = guess.copy()
     # gauss-legrendre quadrature
-    x, w = get_gauss_legendre_quadrature(8) 
-    theta = -np.pi / 2 * (x - 1)
-    z = e_c + e_r * np.exp(1j * theta)
+    x, w = get_gauss_legendre_quadrature(16) 
+    theta = -np.pi * x
+    #theta = np.concatenate((-np.pi * x, np.pi * x))
+    #w = np.concatenate((w, w))
+    # the quadrature points
+    z = (e_c*1j + e_r * np.exp(1j * theta))*dt
 
     # start iteratons
     e_norm_prev = 1e10
-    for iter in range(eom.max_cycle):
+    # solve for the linear system (z-H)Q = Y at z = z_e
+    Q = [np.zeros(size, dtype=complex) for _ in range(nroots)]
+    for e in range(len(z)):
+        print_logging_info(f"e = {e}, z = {z[e]}, theta = {theta[e]}, w = {w[e]}", level=1)
+        Qe = eom._gcrotmk(z[e], b=u_vec[0], diag=diag, precond=precond, 
+                          phase=np.exp(z[e]), is_rt=True, dt=dt)
 
-        ntrial = len(u_vec)
-
-        Q = [np.zeros(size, dtype=complex) for _ in range(ntrial)]
-
-        time_iter_init = time.time()
-        #u_vec = QR(u_vec)    
-        # solve for the linear system (z-H)Q = Y at z = z_e
-        for e in range(len(z)):
-            print_logging_info(f"e = {e}, z = {z[e]}, theta = {theta[e]}, w = {w[e]}", level=1)
-            for l in range(ntrial):
-                Qe = eom._gcrotmk(z[e], b=u_vec[l], diag=diag, precond=precond)
-
-                Q[l] -= w[e]/2 * np.real(e_r * np.exp(1j * theta[e]) * Qe)
+        Q[0] -= w[e]/2 * (e_r * dt * np.exp(1j * theta[e]) * Qe)
         
-        # compute the projected Hamiltonian
-        H_proj = np.zeros((ntrial, ntrial), dtype=complex)
-        B = np.zeros(H_proj.shape, dtype=complex)
-        Hu = [np.zeros(size) for _ in range(ntrial)]
-        Hu = matvec(Q)
-        for i in range(ntrial):
-            for j in range(i):
-                H_proj[j, i] = np.dot(np.conj(Q[j]), Hu[i])
-                H_proj[i, j] = np.dot(np.conj(Q[i]), Hu[j]) 
-                B[i, j] = np.dot(np.conj(Q[i]), Q[j])
-                B[j, i] = B[i, j]
-            H_proj[i, i] = np.dot(np.conj(Q[i]), Hu[i])
-            B[i, i] = np.dot(np.conj(Q[i]), Q[i])
-        # solve the eigenvalue problem
-        eigvals, eigvecs = eig(H_proj, B)
-        # filter out the valid eigenvalues whose real values are within the range of [e_c - e_r, e_c + e_r]
-        valid_inds = np.logical_and(np.real(eigvals) > e_c - e_r, np.real(eigvals) < e_c + e_r)
-        valid_eigvals = eigvals[valid_inds].real
-        # get the eigenvectors corresponding to the max and min eigenvalues
+    u_norm= np.dot(np.conj(Q[0]), Q[0])
+    print_logging_info("Norm of new u vec before normalization = ", u_norm)
+    Q /= np.linalg.norm(Q)
+    u_norm= np.dot(np.conj(Q[0]), Q[0])
+    print_logging_info("Norm of new u vec after normalization = ", u_norm)
+    u_vec = Q
+    time_end = time.time()
+    print_logging_info(f"RT-EOM-CCSD finished in {time_end - time_init:.2f} seconds.", level=0)
 
-        # update u_singles and u_doubles and to the trial vectors
-        for l in range(ntrial):
-            for i in range(len(eigvals)):
-                u_vec[l] += np.real(eigvecs[i, l] * Q[i])
-        
-        # check convergence
-        e_norm = np.linalg.norm(valid_eigvals)
-        if np.abs(e_norm - e_norm_prev) < eom.conv_tol:
-            break
-        else:
-            if iter%4 == 0:
-                max_eigval = np.max(valid_eigvals)
-                min_eigval = np.min(valid_eigvals)
-                max_eigvec = eigvecs[:, np.argmax(valid_eigvals)].dot(np.asarray(Q))
-                min_eigvec = eigvecs[:, np.argmin(valid_eigvals)].dot(np.asarray(Q))
-                # add more trial u vectors based on the max and min eigenvectors
-                u_vec.append(max_eigvec/(max_eigval - diag))
-                u_vec.append(min_eigvec/(min_eigval - diag))
-                print_logging_info(f"Added two more trial vectors, total ntrial= {len(u_vec)}", level=1)
-            print_logging_info(f"Iter = {iter}, All eigenvalues: {eigvals}", level=1)
-            print_logging_info(f"Valid eigenvalues: {valid_eigvals}", level=1)
-            print_logging_info(f"Norm of eigenvalues: {e_norm}, Difference: {np.abs(e_norm - e_norm_prev)}", level=1)
+    return u_vec[0]
 
-        e_norm_prev = e_norm
-
-
-        time_end = time.time()
-        print_logging_info(f"FEAST-EOM-CCSD finished in {time_end - time_init:.2f} seconds.", level=0)
-
-    return eigvals, u_vec
-
-def QR(u):
-    """
-    QR decomposition of a matrix u
-    """
-    u = np.asarray(u).T
-    Q, R = np.linalg.qr(u)
-    Q = Q.T
-    u = []
-    for i in range(len(Q)):
-        u.append(Q[i])
-    return u
     
-class FEAST_EOMEESinglet(EOMEE):
+class CIFRT_EOMEESinglet(EOMEE):
 
     def __init__(self, cc):
         EOMEE.__init__(self, cc)
         self.ls_max_iter = 100
         self.ls_conv_tol = 1e-4
     
-    kernel = feast
+    kernel = kernel 
     matvec = eeccsd_matvec_singlet
 
     def get_init_guess(self, nroots=1, koopmans=False, diag=None):
@@ -167,8 +113,11 @@ class FEAST_EOMEESinglet(EOMEE):
         if x0 is None:
             x0 = np.zeros(size, dtype=complex)
 
+        b_ = b.copy()
+        # convert b_ to complex
         if phase is not None:
-            b *= phase
+            b_ = b_.astype(complex)
+            b_ *= phase
         
         if imds is None:
             self._cc.t2 = self._cc.t2.real
@@ -196,7 +145,7 @@ class FEAST_EOMEESinglet(EOMEE):
         combined_diag = 1./(ze-diag+0.01)
         M = diags(combined_diag, offsets=0)
 
-        Qe_vec, exit_code = gcrotmk(A, b, x0=x0, M=M, maxiter=self.ls_max_iter, tol=self.ls_conv_tol)
+        Qe_vec, exit_code = gcrotmk(A, b_, x0=x0, M=M, maxiter=self.ls_max_iter, tol=self.ls_conv_tol)
         print_logging_info("Linear Solver Info = ", exit_code, level=2)
         return Qe_vec
 
---
+++ pymes/test/test_rt_eom_ccsd/test_cifrt.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+from pyscf import gto, scf, cc
+from pymes.util import fcidump
+from pymes.solver import rt_eom_rccsd 
+from pymes.integral.partition import part_2_body_int
+
+def driver():
+    mol = gto.Mole(
+        atom = 'O	0.0000	0.0000	0.1173; H	0.0000	0.7572	-0.4692; H	0.0000	-0.7572	-0.4692',
+        basis = 'sto6g',
+        verbose = 4,
+        unit = 'A'
+    )
+    mol.build()
+
+    # RHF calculation
+    mf = scf.RHF(mol)
+    mf.kernel()
+
+    # RCCSD calculation
+    mycc = cc.CCSD(mf)
+    mycc.kernel()
+    #mycc.max_memory = 12000
+    mycc.incore_complete = True
+    e, _ = mycc.eomee_ccsd_singlet(nroots=20)
+    #e, _ = mycc.eeccsd(nroots=28)
+    #print(e)
+    np.save("eom_ccsd_pyscf.npy", e)
+
+    # EOM-EE-CCSD calculation
+    eom = rt_eom_rccsd.CIFRT_EOMEESinglet(mycc)
+    eom.max_cycle = 20
+    eom.ls_max_iter = 10
+    eom.conv_tol = 1e-6
+
+    u_vec = np.random.random(eom.vector_size())-0.5
+    u_vec = u_vec/np.linalg.norm(u_vec)
+
+
+   
+    nt = 200
+    dt = 0.5
+    c_t = np.zeros(nt-1, dtype=complex)
+    t = np.arange(1,nt)*dt
+    u0 = u_vec.copy()
+    for n in range(0,nt-1):
+        u_vec_t = eom.kernel(guess=[u_vec], e_c=1.2, e_r=0.05, dt=dt)
+        # update the u_singles and u_doubles
+        ct_ = np.dot(u0, u_vec)
+        u_vec = u_vec_t.copy()
+        print("ct = ", ct_)
+        c_t[n] = ct_
+        np.save("ct.npy", np.column_stack((t,c_t)))
+
+
+def signal_processing():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from scipy.signal import find_peaks
+    from scipy.optimize import curve_fit
+    
+    # Assuming ct[:,0] contains the time data and ct[:,1] contains the signal data
+    time_data = ct[:, 0]
+    signal_data = ct[:, 1]
+    
+    # Compute the sampling rate (assuming uniform sampling)
+    delta_t = time_data[1] - time_data[0]
+    
+    # FFT of the signal
+    ct_fft = np.fft.fft(signal_data)
+    ct_fft = ct_fft / len(ct_fft)  # Normalize the FFT
+    ct_fft = ct_fft * 2
+    ct_fft[0] = ct_fft[0]
+    #ct_fft = np.fft.fftshift(ct_fft)
+    
+    # Compute the frequency array and convert to angular frequency
+    ct_freq = np.fft.fftfreq(len(ct_fft), d=delta_t)
+    #positive_freq_indices = ct_freq >= 0
+    #ct_freq = ct_freq[positive_freq_indices]
+    #ct_fft = ct_fft[positive_freq_indices]
+    
+    ct_omega =  (2 * np.pi * ct_freq)
+    ct_omega = (np.fft.fftshift(ct_omega)).real
+    # Identify the peaks
+    peaks, _ = find_peaks(np.abs(ct_fft), height=0.005)
+    print(peaks)
+    print(np.real(ct_omega[peaks]))
+    
+    
+    # Plot the FFT with the correct x-axis
+    plt.figure()
+    #plt.plot(ct_omega, np.abs(ct_fft.real), '--', label='FFT real')
+    #plt.plot(ct_omega, np.abs(ct_fft.imag), '--', label='FFT imag')
+    plt.plot(ct_omega, np.abs(ct_fft), label='FFT magnitude')
+    # Plot the fitted composite Lorentzian function
+    # Define the Lorentzian function
+    def composite_lorentzian(x, *params):
+        n_peaks = len(params) // 3
+        y = np.zeros_like(x)
+        for i in range(n_peaks):
+            A = params[3 * i]
+            x0 = params[3 * i + 1]
+            gamma = params[3 * i + 2]
+            y += (A / np.pi) * (gamma / ((x - x0)**2 + gamma**2))
+        return y
+    
+    # Initial guess for the parameters [A1, x01, gamma1, A2, x02, gamma2, ...]
+    #initial_guess = []
+    #for peak in peaks:
+    #    initial_guess.extend([np.real(ct_fft)[peak], np.abs((ct_omega[peak])), 0.01])
+    #
+    ### Fit the composite Lorentzian function
+    #popt, _ = curve_fit(composite_lorentzian, np.real(ct_omega), np.abs((ct_fft)), p0=initial_guess)
+    #fitted_peak_locations = popt[1::3]
+    ## Place a vertical line at the fitted peak locations
+    #plt.vlines(fitted_peak_locations, 0, np.max(np.abs(ct_fft)), color='g', linestyle='--', label='Fitted Peaks')
+    #print("Fitted peaks = ", popt[popt>1])
+    #plt.plot(ct_omega, composite_lorentzian(ct_omega, *popt), label='Composite Lorentzian fit')
+    
+    plt.xlim(1, 3)
+    plt.ylim(0, 0.03)
+    plt.xlabel('Angular Frequency (rad/s)')
+    plt.ylabel('Magnitude')
+    plt.legend()
+    plt.show()
+if __name__ == "__main__":
+    #test_rt_eom_ccsd_model_ham()
+    driver()
