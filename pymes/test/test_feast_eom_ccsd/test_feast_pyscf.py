a03d1bfe93183dfd2f14fb0bbafc97d68abaa80d
keliao <ke.liao.whu@gmail.com>
2024-09-07 23:59:19 +0200

    add user trial

--- pymes/test/test_feast_eom_ccsd/test_feast_pyscf.py
+++ pymes/test/test_feast_eom_ccsd/test_feast_pyscf.py
@@ -14,6 +14,7 @@ def driver():
         atom = 'O 0.0000	0.0000	0.1185; H 0.0000	0.7555	-0.4739; H 0.0000 -0.7555 -0.4739',
         basis = basis,
         verbose = 4,
+        symmetry = True,
         unit = 'A'
     )
     mol.build()
@@ -37,7 +38,7 @@ def driver():
 
     # EOM-EE-CCSD calculation
     eom = feast_eom_rccsd.FEAST_EOMEESinglet(mycc)
-    logger.verbose = 5 
+    logger.verbose = 2
     eom.max_cycle = 20
     eom.ls_max_iter = 1
     eom.conv_tol = 1e-7
@@ -47,11 +48,14 @@ def driver():
     # constructing guess
     nocc = mycc.nocc
     nvir = mycc.nmo - nocc
-    vec_size = eom.vector_size()
-    guess = np.zeros(vec_size)
-    guess[nvir] = 1.0
+    r1 = np.zeros((nocc, nvir))
+    r2 = np.zeros((nocc, nocc, nvir, nvir))
+    r1[1, 0] = -1.0
+    #r1[0, 5] = 
+    guess = mycc.amplitudes_to_vector(r1, r2)
+    print(" loc = ", np.argmax(np.abs(guess)))
     emin = 1.1441
-    de = 0.02
+    de = 0.5
     emax = emin + de*0.99999
     energies = []
     r1 = []
@@ -59,18 +63,19 @@ def driver():
     for emin_ in np.arange(emin, emax, de):
         print("emin = ", emin_, "emax = ", emin_+de)
     
-        e_feast, u_vecs = eom.kernel(nroots=1,  emin=emin_, emax=emin_+de, ngl_pts=8, guess=[guess])
+        e_feast, u_vecs = eom.kernel(nroots=4,  emin=emin_, emax=emin_+de, ngl_pts=8, guess=[guess])
         for u in u_vecs:
             r1_, r2_ = eom.vector_to_amplitudes(u)
             r1.append(r1_)
             r2.append(r2_)
-        print("r1 norm: ", np.linalg.norm(np.asarray(r1)))
+        print("r1 norm: ", np.linalg.norm(np.asarray(r1), axis=(1,2)))
         np.save(f"eom_ccsd_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", e_feast)
         np.save(f"r2_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", np.asarray(r2))
         np.save(f"r1_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", np.asarray(r1))
         energies.append(e_feast)
-    print("energies: ", energies)
-    print("valid targets: ", e[np.logical_and(e > emin, e < emax)])
+    print("feast energies: ", energies)
+    #print("valid targets: ", e[np.logical_and(e > emin, e < emax)])
+    print("all energies : ", e)
 
 def main():
     driver()
