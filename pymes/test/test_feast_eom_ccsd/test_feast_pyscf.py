e4c15fa720df4a270efe5cd9bf45ee3806c20366
keliao <ke.liao.whu@gmail.com>
2024-09-07 19:37:55 +0200

    Try adding user guess

--- pymes/test/test_feast_eom_ccsd/test_feast_pyscf.py
+++ pymes/test/test_feast_eom_ccsd/test_feast_pyscf.py
@@ -8,8 +8,8 @@ from pymes.solver import feast_eom_rccsd
 
 
 def driver():
-    #basis = '6311g**'
-    basis = 'ccpvtz'
+    basis = '6311g**'
+    #basis = 'ccpvtz'
     mol = gto.Mole(
         atom = 'O 0.0000	0.0000	0.1185; H 0.0000	0.7555	-0.4739; H 0.0000 -0.7555 -0.4739',
         basis = basis,
@@ -29,41 +29,48 @@ def driver():
     mycc.kernel()
     #mycc.max_memory = 12000
     mycc.incore_complete = True
-    #e, _ = mycc.eomee_ccsd_singlet(nroots=28)
+    #e, _ = mycc.eomee_ccsd_singlet(nroots=40)
+    #np.save("eom_ccsd_pyscf_all.npy", e)
     ##e, _ = mycc.eeccsd(nroots=28)
     ##print(e)
-    #e = np.load("eom_ccsd_pyscf_all.npy")
+    e = np.load("eom_ccsd_pyscf_all.npy")
 
     # EOM-EE-CCSD calculation
     eom = feast_eom_rccsd.FEAST_EOMEESinglet(mycc)
     logger.verbose = 5 
-    eom.max_cycle = 100
-    eom.ls_max_iter = 3
+    eom.max_cycle = 20
+    eom.ls_max_iter = 1
     eom.conv_tol = 1e-7
     eom.max_ntrial = 7
-    #eom.verbose = 5
+    eom.verbose = 5
     eom.nroots = 7
-
-    emin = 1.15
-    emax = 1.19999
-    de = 0.05
+    # constructing guess
+    nocc = mycc.nocc
+    nvir = mycc.nmo - nocc
+    vec_size = eom.vector_size()
+    guess = np.zeros(vec_size)
+    guess[nvir] = 1.0
+    emin = 1.1441
+    de = 0.02
+    emax = emin + de*0.99999
     energies = []
     r1 = []
     r2 = []
     for emin_ in np.arange(emin, emax, de):
         print("emin = ", emin_, "emax = ", emin_+de)
     
-        e_feast, u_vecs = eom.kernel(nroots=4,  emin=emin_, emax=emin_+de, ngl_pts=8)
+        e_feast, u_vecs = eom.kernel(nroots=1,  emin=emin_, emax=emin_+de, ngl_pts=8, guess=[guess])
         for u in u_vecs:
             r1_, r2_ = eom.vector_to_amplitudes(u)
             r1.append(r1_)
             r2.append(r2_)
+        print("r1 norm: ", np.linalg.norm(np.asarray(r1)))
         np.save(f"eom_ccsd_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", e_feast)
         np.save(f"r2_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", np.asarray(r2))
         np.save(f"r1_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", np.asarray(r1))
         energies.append(e_feast)
     print("energies: ", energies)
-    #print("valid targets: ", e[np.logical_and(e > emin, e < emax)])
+    print("valid targets: ", e[np.logical_and(e > emin, e < emax)])
 
 def main():
     driver()
