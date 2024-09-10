69b91fbd8e1a2a1b06b7bbb50b101694f3ad676c
Ke Liao <ke.liao.whu@gmail.com>
2024-09-10 17:00:47 +0200

    clean up code, change interface of feast()

--- pymes/test/test_feast_eom_ccsd/test_feast_pyscf.py
+++ pymes/test/test_feast_eom_ccsd/test_feast_pyscf.py
@@ -38,47 +38,25 @@ def driver():
 
     # EOM-EE-CCSD calculation
     eom = feast_eom_rccsd.FEAST_EOMEESinglet(mycc)
-    logger.verbose = 2
     eom.max_cycle = 20
     eom.ls_max_iter = 1
     eom.conv_tol = 1e-7
     eom.max_ntrial = 7
-    eom.verbose = 5
-    eom.nroots = 7
+    #eom.verbose = 5
     # constructing guess
-    nocc = mycc.nocc
-    nvir = mycc.nmo - nocc
-    #r1 = (np.random.random((nocc, nvir))-0.5)*0.0001
-    #r2 = (np.random.random((nocc, nocc, nvir, nvir)) - 0.5)*0.00001
-    r1 = np.zeros((nocc, nvir))
-    r2 = np.zeros((nocc, nocc, nvir, nvir))
-    r1[0, 0] = 1
+    #nocc = mycc.nocc
+    #nvir = mycc.nmo - nocc
+    #r1 = np.zeros((nocc, nvir))
+    #r2 = np.zeros((nocc, nocc, nvir, nvir))
+    #r1[0, 0] = 1
     #r1 /= np.linalg.norm(r1)
     #r1[0, 5] = 
-    guess = mycc.amplitudes_to_vector(r1, r2)
-    print(" loc = ", np.argmax(np.abs(guess)))
-    emin = 1.1441
+    #guess = mycc.amplitudes_to_vector(r1, r2)
+    #print(" loc = ", np.argmax(np.abs(guess)))
     de = 3
-    emax = emin + de*0.99999
-    energies = []
-    r1 = []
-    r2 = []
-    for emin_ in np.arange(emin, emax, de):
-        print("emin = ", emin_, "emax = ", emin_+de)
-    
-        e_feast, u_vecs = eom.kernel(nroots=4,  emin=emin_, emax=emin_+de, ngl_pts=8, guess=[guess], e_guess_init=19.66)
-        for u in u_vecs:
-            r1_, r2_ = eom.vector_to_amplitudes(u)
-            r1.append(r1_)
-            r2.append(r2_)
-        print("r1 norm: ", np.linalg.norm(np.asarray(r1), axis=(1,2)))
-        np.save(f"eom_ccsd_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", e_feast)
-        np.save(f"r2_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", np.asarray(r2))
-        np.save(f"r1_feast_{emin_:.4f}_{(emin_+de):.4f}.{basis}.npy", np.asarray(r1))
-        energies.append(e_feast)
-    print("feast energies: ", energies)
-    #print("valid targets: ", e[np.logical_and(e > emin, e < emax)])
-    #print("all energies : ", e)
+    e_feast, _ = eom.kernel(nroots=2, ngl_pts=8, e_c=19.66, e_r=de, e_brd=1.01)
+    print("feast energies: ", e_feast)
+    assert np.isclose(e_feast[0].real, 19.68806362) 
 
 def main():
     driver()
